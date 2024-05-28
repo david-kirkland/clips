f85655d2dae497b76430d8a3fb206ace84964899
David Kirkland <6099248+david-kirkland@users.noreply.github.com>
2024-05-27 20:28:13 -0400

    added shared modal component and service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
+  constructor(public modal: ModalService) {}
 
+  openModal($event: Event) {
+    $event.preventDefault();
+    this.modal.toggleModal();
+  }
 }
