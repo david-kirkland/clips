3c0fb045efe555b6bee0dc9e85ccf8716b884c40
David Kirkland <6099248+david-kirkland@users.noreply.github.com>
2024-05-27 20:53:05 -0400

    fix modal singleton issue with IDs

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
-  //providers: [ModalService],
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = '';
+
   constructor(public modal: ModalService) {}
 
   ngOnInit(): void {}
 
   closeModal() {
-    this.modal.toggleModal();
+    this.modal.toggleModal(this.modalID);
   }
 }
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
-  styleUrl: './auth-modal.component.scss'
+  styleUrl: './auth-modal.component.scss',
 })
-export class AuthModalComponent {
+export class AuthModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
 
+  ngOnInit(): void {
+    this.modal.register('authentication');
+  }
 }
